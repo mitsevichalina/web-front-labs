7d9823048be14c6a841765aab26d08729ecc98be
Мицевич Алина <9133880308@mail.ru>
2024-05-25 20:08:43 +0700

    аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -3,5 +3,8 @@ function showTime(){
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
     document.querySelector('.seconds').innerHTML = currentTime;
+
+    time = document.getElementById('second-arrow')
+    time.style.transform = `rotate(${new Date().getSeconds() * 6}deg) translate(0, 0px)`;
 }
 setInterval(showTime, 1000);
