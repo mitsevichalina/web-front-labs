e67bb8e6731e8ab5cae9452fd935d7434b10aaff
Мицевич Алина <9133880308@mail.ru>
2024-05-25 14:18:30 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -5,6 +5,10 @@ function showDate() {
     let out4 = document.querySelector('.date4');
     let out5 = document.querySelector('.date5');
     let out6 = document.querySelector('.date6');
+    let year = document.querySelector('.year');
+    let month = document.querySelector('.month');
+    let date = document.querySelector('.date');
+    let week = document.querySelector('.week');
 
     let today = new Date();
     out1.innerHTML = 'Дата и время для русской локали: ' + today.toLocaleString('ru-RU');
@@ -13,4 +17,13 @@ function showDate() {
     out4.innerHTML = 'Дата и время для турецкой локали: ' + today.toLocaleString('tr-TR');
     out5.innerHTML = 'Дата и время для шведской локали: ' + today.toLocaleString('sv-SE');
     out6.innerHTML = 'Дата и время для норвежской локали: ' + today.toLocaleString('nn-NO');
+    year.innerHTML = 'Текущий год: ' + today.getFullYear();
+    nowMonth = today.getMonth();
+    const MonthD = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь', ]
+    month.innerHTML = 'Текущий месяц: ' + MonthD[nowMonth];
+    let nowDate = today.getDate();
+    date.innerHTML ='Текущая дата: ' +  nowDate;
+    nowWeek = today.getDay();
+    const WeekD = ['Воскресенье', 'Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
+    week.innerHTML = 'День недели: ' + WeekD[nowWeek];
 }
